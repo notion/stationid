82471a4108b43d4680da8b57cb72b8765fe98b0f
amdayton <amdayton@gmail.com>
2011-04-10 00:52:42 -0400

    initial commit

--- sapphire/javascript/ConfirmedPasswordField.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-	$('.confirmedpassword .showOnClick a').live('click', function () {
-		$('.showOnClickContainer', $(this).parent()).toggle('fast');
-		return false;
-	});
-})(jQuery);
